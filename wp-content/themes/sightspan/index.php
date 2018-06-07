f725aacc92cb484eece42ed3867d7b0e66354497
Fahad <webdevelopment@sightspan.com>
2018-06-07 15:10:04 +0500

    All code

---
+++ wp-content/themes/sightspan/index.php
@@ -0,0 +1,144 @@
+<?php get_header();?>
+
+<section id="mainContent" class="fullPage">
+  <?php get_sidebar('topbanner');?>
+  <div class="container">
+    <div class="home__element home__solutions-container">
+      <h1 class="stack-nav-content__title home__subtitle">Global Risk Management</h1>
+      <!--
+			 <p class="about-us__dxc-content">Our network of global risk management experts work to provide innovative and cost effective solutions that help embed appropriate risk management disciplines into organizations???
+            </p> 
+			-->
+      <div class="row stackable-row">
+        <div class="col-sm-3">
+          <div class="stack-nav">
+            <?php 
+			$categories	=	get_categories(array('hide_empty' => 0, 'orderby'=>'id','order'=>'asc', 'child_of' => 2, 'depth' => 0, 'show_count' => 0, 'parent' => '2'));
+			$i=0;
+			foreach($categories as $category){
+            if($i==0){
+            	$active	=	"active";
+            }else{
+            	$active	=	"";
+            }
+			$i = $i+1;
+            if($i==1){
+				$link	=	get_site_url()."/industries/";
+			}elseif($i==2){
+				$link	=	get_site_url()."/aml-ctf/";
+			}elseif($i==3){
+				$link	=	get_site_url()."/compliance/";
+			}elseif($i==4){
+				$link	=	get_site_url()."/psanctions/";
+			}elseif($i==5){
+				$link	=	get_site_url()."/security/";
+			}elseif($i==6){
+				$link	=	get_site_url()."/ptraining/";
+			}elseif($i==7){
+				$link	=	get_site_url()."/ptechnology/";
+			}elseif($i==8){
+				$link	=	get_site_url()."/ssi/";
+			}
+			?>
+            
+            <a class="btn0 btn0-link <?php #echo $active; ?>" href="<?php echo $link; ?>">
+            <button class="btn btn-stack"><?php echo $category->name;?></button>
+            </a>
+            <?php } ?> </div>
+        </div>
+        <!-- Industries -->
+        <div class="col-md-9 stack-nav-content active">
+          <button class="btn btn-stack stack-nav-inset"> Industries </button>
+          <div class="content-container font--base">
+            <div class="list--media-desc list--media-desc--sm-stack clearfix">
+              <div class="header-block"> </div>
+              <div class="row list--sm-flex-wrap">
+                <?php 
+			$categories	=	get_categories(array('hide_empty' => 0, 'orderby'=>'id','order'=>'asc', 'child_of' => 3, 'depth' => 0, 'show_count' => 0, 'parent' => '3'));
+			foreach($categories as $category){
+            ?>
+            <div class="col-sm-4 list--media-desc__col list--media-desc__col--sm-stacked">
+                  <div class="list--media-desc__item">
+                  <img class="img-responsive" src="<?php echo z_taxonomy_image_url($category->term_id); ?>" /><a class="link--btn-next strong list--media-desc__link" href="<?php echo get_site_url(); ?>/industries/"> <?php echo $category->name;?> </a> </div>
+                </div>
+                <?php } ?>
+              </div>
+            </div>
+          </div>
+        </div>
+        <?php for($l=4;$l<11;$l++){
+            if($l==3){
+				$link	=	get_site_url()."/industries/";
+			}elseif($l==4){
+				$link	=	get_site_url()."/aml-ctf/";
+			}elseif($l==5){
+				$link	=	get_site_url()."/compliance/";
+			}elseif($l==6){
+				$link	=	get_site_url()."/psanctions/";
+			}elseif($l==7){
+				$link	=	get_site_url()."/security/";
+			}elseif($l==8){
+				$link	=	get_site_url()."/ptraining/";
+			}elseif($l==9){
+				$link	=	get_site_url()."/ptechnology/";
+			}elseif($l==10){
+				$link	=	get_site_url()."/ssi/";
+			}
+		?>
+        <!-- Analytics -->
+        <div class="col-md-9 stack-nav-content">
+          <?php $single_cat	=	get_category($l);?>
+          <button class="btn btn-stack stack-nav-inset"> <?php echo $single_cat->name;?> </button>
+          <div class="content-container font--base">
+            <div class="list--media-desc clearfix">
+              <div class="header-block">
+                <div class="header-block__hero" style="background: url(<?php echo z_taxonomy_image_url($single_cat->term_id); ?>) center no-repeat; height: 350px;width: 850px;"> </div>
+                <h2 class="header-block__heading strong"> <?php echo $single_cat->category_description;?> </h2>
+              </div>
+              <a class="link--btn-next header-block__link" href="<?php echo $link;?>"> Learn More </a> </div>
+          </div>
+        </div>
+        <?php } ?>
+      </div>
+    </div>
+    <div class="container">
+      <div class="our-mission home__element">
+        <h1 class="home__subtitle">About Us</h1>
+        <div class="our-mission__content content-swap" data-spy="contentswap">
+          <div class="our-mission__content__left content-swap__area active">
+            <h2 class="our-mission__content__subtitle strong">Company Profile</h2>
+            <div> <a id="swapper" class="arrow-link js-swapper content-swap__show-more">More</a> </div>
+            <p class="content-swap__show-if-active"><?php
+    $the_query	=	new WP_Query('page_id=92');
+	while($the_query->have_posts()) : $the_query->the_post();
+		the_excerpt();
+	endwhile;
+	?><a href="<?php echo get_site_url(); ?>/about-us/" class="link--btn-next our-mission__content__link strong">Learn more</a> </p>
+          </div>
+          <div class="our-mission__content__right content-swap__area">
+            <p class="content-swap__show-if-active">We lead clients through accelerating change, helping them harness the power of technology to deliver new outcomes from their business. Our values are CLEAR. <a href="about_us/ds/29625-our_mission_and_values.html" class="link--btn-next our-mission__content__link strong">Learn more</a> </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="logos-component home__element">
+        <h1 class="home__subtitle">SightSpan Partner Network</h1>
+        <div class="logos-component__logos-container">
+          <div class="logos-component__img-container"> <a href="amazon.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/logo-amazon.png"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="att.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/logo-att.png"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="dell_emc.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/logo-dell.png"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="partner_network/alliances/32238-ibm.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/logo-ibm.png"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="lenovo.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/logo-lenovo.png"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="partner_network.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/logo-hcl.png"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="partner_network/alliances/143969-hewlett_packard_enterprise.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/logo-hpe.png"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="partner_network.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/logo-microfocus.png"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="partner_network.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/knightscope_logo.jpg"/> </a> </div>
+          <div class="logos-component__img-container"> <a href="partner_network.html"> <img class="img-responsive logos-component__image" src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/smartsense.png"/> </a> </div>
+        </div>
+        <div class="btn-wrapper clearfix"> <a class="btn btn-primary strong pull-right btn--wider text-uppercase" href="partners.html"> EXPLORE SIGHTSPAN PARTNER NETWORKS </a> </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php get_footer();?>
---
+++ wp-content/themes/sightspan/sidebar-topbanner.php
@@ -0,0 +1,45 @@
+<article class="pageContent" itemprop="mainContentOfPage">
+    <div class="hero-carousel right-slick-dots"> 
+      
+      <!--billboard # -->
+      
+      <div class="hero-carousel__item hero-carousel__item--rounded ">
+        <div class="hero-carousel__small-container">
+          <div class="hero-carousel__text-container hero-carousel__text-container--rounded">
+            <h1 class="hero-carousel__title">AML / Compliance</h1>
+          </div>
+        </div>
+        <video muted autoplay loop class="hero-carousel__video">
+          <source src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/video1.mp4" type="video/mp4" width="600px" height="600px">
+        </video>
+      </div>
+      <!--billboard # end--> 
+      
+      <!--billboard #-->
+      <div class="hero-carousel__item hero-carousel__item--rounded ">
+        <div class="hero-carousel__small-container">
+          <div class="hero-carousel__text-container hero-carousel__text-container--rounded">
+            <h1 class="hero-carousel__title">Cyber Security</h1>
+          </div>
+        </div>
+        <video muted autoplay loop class="hero-carousel__video">
+          <source src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/video2.mp4" type="video/mp4">
+        </video>
+      </div>
+      <!--billboard # end--> 
+      
+      <!--billboard #-->
+      <div class="hero-carousel__item hero-carousel__item--rounded ">
+        <div class="hero-carousel__small-container">
+          <div class="hero-carousel__text-container hero-carousel__text-container--rounded">
+            <h1 class="hero-carousel__title">Analytical Based Risk Management</h1>
+          </div>
+        </div>
+        <video muted autoplay loop class="hero-carousel__video">
+          <source src="<?php echo get_site_url(null, '/wp-content/themes/sightspan'); ?>/assets/images/video3.mp4" type="video/mp4">
+        </video>
+      </div>
+      <!--billboard # end--> 
+      
+    </div>
+  </article>
