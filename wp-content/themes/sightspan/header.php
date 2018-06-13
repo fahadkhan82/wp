3da66aeef8cf0742ea0bcef832be287af3721b5d
Fahad <webdevelopment@sightspan.com>
2018-06-13 11:44:16 +0500

    Fixing youtube

--- wp-content/themes/sightspan/header.php
+++ wp-content/themes/sightspan/header.php
@@ -30,6 +30,7 @@
 <?php #bloginfo("name");?>
 </title>
 <?php wp_head();?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript">
             var responsive=true ;
         </script>
